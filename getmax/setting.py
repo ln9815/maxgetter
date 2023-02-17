ab6e4c30efc54336bdf206f1b654994b5705b5a9
victor yang <mingxiangy@126.com>
2023-02-17 08:53:02 +0800

    remove secrets.

--- getmax/setting.py
+++ getmax/setting.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 class BaseConfig(object):
     MAX_HOST = "https://world.maxmara.com"
     LOGGING_LEVEL = logging.DEBUG
-    FOLDER_SAVE = "D:\\MaxmaraSave\\"
+    FOLDER_SAVE = None
 
 
 class DevelopmentConfig(BaseConfig):
@@ -20,13 +20,6 @@ class TestingConfig(BaseConfig):
 
 
 class ProductionConfig(BaseConfig):
-    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:aA12345678@124.70.214.80:3306/max_db"
-    # PS> $env:DB_USER="root"
-    # PS> $env:DB_PASSWORD="aA12345678"
-    # PS> $env:DB_HOST="124.70.214.80"
-    # PS> $env:DB_PORT="3306"
-    # PS> $env:DB_DATABASE="max_db"
-    # PS> $env:FOLDER_SAVE="D:\\MaxmaraSave\\"
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{}:{}@{}:{}/{}".format(
         os.getenv("DB_USER"),
         os.getenv("DB_PASSWORD"),
