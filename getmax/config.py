0859e580bf3d04456217b217a37313cc6b2a929f
victor yang <mingxiangy@126.com>
2023-02-22 13:45:14 +0800

    update for settting

--- getmax/config.py
+++ getmax/config.py
@@ -3,7 +3,7 @@ import logging
 from dynaconf import Dynaconf
 
 logger = logging.getLogger(__name__)
-
+settings = None
 
 def get_instance_path():
     instance_path = None
@@ -27,18 +27,14 @@ def get_settings_files():
 
     fn_setting = os.path.join(instance_path, 'settings.toml')
     fn_secrets = os.path.join(instance_path, 'secrets.toml')
-    if not os.path.exists(fn_setting):
-        logger.warning(f'setting file {fn_setting} doese not exist.')
-        assert os.path.exists(fn_setting)
-    if not os.path.exists(fn_secrets):
-        logger.warning(f'setting file {fn_secrets} doese not exist.')
-        assert os.path.exists(fn_secrets)
-        
+    if not os.path.exists(fn_setting) or not os.path.exists(fn_secrets):
+        return None
+
     return [fn_setting,fn_secrets]
-
-settings = Dynaconf(
-    envvar_prefix="DYNACONF",
-    settings_files=get_settings_files(),
-)
-settings.INSTANCE_PATH = get_instance_path()
-
+_setting_files = get_settings_files()
+if _setting_files:
+    settings = Dynaconf(
+        envvar_prefix="DYNACONF",
+        settings_files=_setting_files,
+    )
+    settings.INSTANCE_PATH = get_instance_path()
--- getmax/scripts/runmax.py
+++ getmax/scripts/runmax.py
@@ -2,31 +2,38 @@ import click
 import logging
 import os
 import sys
-from getmax.downloader import MaxDownloader
-from getmax.util import logging_init
+
 logger = logging.getLogger(__name__)
 
 
 @click.command()
-@click.option('--workdir', '-w', required=True, type=str, help='work directory for this app.')
-@click.option('--stream', '-s', is_flag=True, help='Stream output for log.')
+@click.option('--workdir', help='work directory for this app.')
 @click.option('--initdb', is_flag=True, help='Init database and data.')
-@click.option('--download', '-d', is_flag=True, help='Downlaod the pending images in database.')
 @click.option('--retrieve', '-r', is_flag=True, help='Retrieve product and image information, and update to database.')
+@click.option('--download', '-d', is_flag=True, help='Downlaod the pending images in database.')
+@click.option('--stream', '-s', is_flag=True, help='Stream output for log.')
 def run(workdir, stream, initdb, download, retrieve):
-    
+
+    old_env = os.getenv('WORK_DIR')
     if workdir:
         if os.path.exists(workdir):
             os.environ['WORK_DIR'] = workdir
         else:
-            print(f'{workdir} does not exists.')
-            return
-    else:
-        if os.environ.get('WORK_DIR') is None:
-            print(f'env var WORK_DIR does not exist.')
-            return
+            print(f'{workdir} does not exists, and no workdir specified.')
+            exit(-1)
+
+    if os.getenv('WORK_DIR') is None:
+        print(f'env var WORK_DIR does not exist.')
+        exit(-1)
 
     from getmax.config import settings
+    from getmax.downloader import MaxDownloader
+    from getmax.util import logging_init
+
+    if settings is None:
+        path = os.getenv('WORK_DIR')
+        print(f'setting files does not exist in {path}.')
+        exit()
 
     logfile = os.path.join(settings.INSTANCE_PATH, settings.LOG_FILE)
     click.echo(f'log file saved on {logfile}.')
@@ -53,6 +60,11 @@ def run(workdir, stream, initdb, download, retrieve):
         click.echo('Start to download pending images.')
         app.download_pending_images()
         click.echo('Pending images in database was downlaoded and saved.')
+
+    if old_env:
+        os.environ['WORK_DIR'] = old_env
+    else:
+        del os.environ['WORK_DIR']
 
 
 def do():
